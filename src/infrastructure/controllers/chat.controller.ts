1326b9052ed7d829132c7f0873eb4008dec386eb
Guilherme-SG <silvagomesguilherme@hotmail.com>
2023-12-04 14:14:44 +0000

    feat: prompt generator

--- src/infrastructure/controllers/chat.controller.ts
+++ src/infrastructure/controllers/chat.controller.ts
@@ -1,6 +1,7 @@
 import { PromptDto } from '@entities/prompt.dto';
 import { Controller, Post, Body } from '@nestjs/common';
 import { ChatFreelanceProposalService } from '@services/gpt/chat-freelance-proposal.service';
+import { ChatGeneratePromptService } from '@services/gpt/chat-generate-prompt';
 import { ChatNestJSProgrammerService } from '@services/gpt/chat-nestjs-programmer.service';
 import { ChatShellService } from '@services/gpt/chat-shell.service';
 
@@ -10,6 +11,7 @@ export class ChatController {
     private readonly chatFreelanceProposalService: ChatFreelanceProposalService,
     private readonly chatNestJSProgrammerService: ChatNestJSProgrammerService,
     private readonly chatShellService: ChatShellService,
+    private readonly chatGeneratePromptService: ChatGeneratePromptService,
   ) {}
 
   @Post("/freelance-proposal")
@@ -26,5 +28,10 @@ export class ChatController {
   @Post("/shell")
   shell(@Body() body: PromptDto ) {
     return this.chatShellService.prompt(body.prompt);
+  }
+
+  @Post("/generate-prompt")
+  generatePrompt(@Body() body: PromptDto ) {
+    return this.chatGeneratePromptService.prompt(body.prompt);
   }
 }
--- src/infrastructure/modules/gpt.module.ts
+++ src/infrastructure/modules/gpt.module.ts
@@ -3,6 +3,7 @@ import { ChatController } from '@controllers/chat.controller';
 import { ChatFreelanceProposalService } from '@services/gpt/chat-freelance-proposal.service';
 import { ChatNestJSProgrammerService } from '@services/gpt/chat-nestjs-programmer.service';
 import { ChatShellService } from '@services/gpt/chat-shell.service';
+import { ChatGeneratePromptService } from '@services/gpt/chat-generate-prompt';
 
 @Module({    
   controllers: [ChatController],
@@ -10,6 +11,7 @@ import { ChatShellService } from '@services/gpt/chat-shell.service';
     ChatFreelanceProposalService,
     ChatNestJSProgrammerService,
     ChatShellService,
+    ChatGeneratePromptService,
   ],
 })
 export class GTPModule {}
