08a29c57a37b6eb7b3ecfca5c44b8104e7f015af
Guilherme-SG <silvagomesguilherme@hotmail.com>
2023-12-10 02:26:17 +0000

    chore: screenshoter

--- src/infrastructure/modules/scrapper.module.ts
+++ src/infrastructure/modules/scrapper.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ScrapperController } from '@controllers/scrapper.controller';
+import { ScrapperController } from '@controllers/scrapper/scrapper.controller';
 import { ScrapperService } from '@services/scrapper/scrapper.service';
 
 @Module({  
--- src/infrastructure/services/scrapper/scrapper.service.ts
+++ src/infrastructure/services/scrapper/scrapper.service.ts
@@ -1,43 +1,41 @@
 import { Injectable } from "@nestjs/common";
-import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer-extra';
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+
 
 @Injectable()
 export class ScrapperService {
     constructor() {}
 
     async get() {
-        const browser = await puppeteer.launch({
-          headless: 'new',
-          args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            ],
-        });
+        const browser = await puppeteer
+          .use(StealthPlugin())
+          .launch({
+            headless: 'new',
+            args: [
+              '--no-sandbox',
+              '--disable-setuid-sandbox',
+              ],
+          });
+
         const page = await browser.newPage();
       
         // Navigate the page to a URL
-        await page.goto('https://developer.chrome.com/');
+        await page.goto('https://www.coingecko.com/pt/moedas/bitcoin');
       
         // Set screen size
         await page.setViewport({width: 1080, height: 1024});
       
-        // Type into search box
-        await page.type('.search-box__input', 'automate beyond recorder');
-      
-        // Wait and click on first result
-        const searchResultSelector = '.search-box__link';
-        await page.waitForSelector(searchResultSelector);
-        await page.click(searchResultSelector);
-      
-        // Locate the full title with a unique string
-        const textSelector = await page.waitForSelector(
-          'text/Customize and automate'
-        );
-        const fullTitle = await textSelector?.evaluate(el => el.textContent);
-      
-        // Print the full title
-        console.log('The title of this blog post is "%s".', fullTitle);
+        // Wait for the required DOM to be rendered
+
+        // Screen capture
+        await page.screenshot({
+          path: 'screenshot.png',
+          fullPage: true,
+        });
       
         await browser.close();
+
+        return { message: "Scrapper started" }
     }
 }
