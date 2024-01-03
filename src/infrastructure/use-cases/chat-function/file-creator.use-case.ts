f30ea51192e9b281a466d465e4474bd1014b5876
Guilherme-SG <silvagomesguilherme@hotmail.com>
2024-01-03 03:50:06 +0000

    feat: loading effect for prompts

--- src/infrastructure/use-cases/chat-function/file-creator.use-case.ts
+++ src/infrastructure/use-cases/chat-function/file-creator.use-case.ts
@@ -9,18 +9,18 @@ import * as readline from 'readline';
 @Injectable()
 export class FileCreatorUseCase implements UseCase, OnModuleInit {
     private rl: readline.Interface;
-    
+
 
     constructor(
         private readonly chatShellUseCase: ChatShellUseCase,
         private readonly osExecuterService: OSExecuterService
     ) {
         this.rl = readline.createInterface({
-            input: process.stdin, 
+            input: process.stdin,
             output: process.stdout,
         });
 
-        
+
     }
     onModuleInit() {
         this.stdin("[Sistema] - Insira um comando: ");
@@ -34,22 +34,37 @@ export class FileCreatorUseCase implements UseCase, OnModuleInit {
         }) as PromptResponse;
 
         this.osExecuterService.execute(shellResponse.content);
-        // this.chatShellUseCase.log()
         console.log(shellResponse)
         return shellResponse;
     }
 
     public async stdin(question: string = "") {
         this.rl.question(question, async (answer) => {
-            if(answer === "exit") {
+            if (answer === "exit") {
                 this.rl.close();
                 process.exit(0);
             }
 
             this.chatShellUseCase.log()
             this.rl.write(`[Você]: ${answer}\n`);
-            this.execute(answer);
+            let processing = this.execute(answer);
+            this.loadingEffect(processing);
+
+            await processing;
             this.stdin(question)
         })
+    }
+
+    private loadingEffect(promiseToWait: Promise<any>) {
+        const P = ['\\', '|', '/', '-'];
+        let x = 0;
+        const loader = setInterval(() => {
+            process.stdout.write(`\r${P[x++]}`);
+            x %= P.length;
+        }, 250);
+
+        promiseToWait.then(() => {
+            clearInterval(loader);
+        });
     }
 }
